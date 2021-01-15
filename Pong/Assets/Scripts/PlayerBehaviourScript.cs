51868a25d929ba86d95cb8025ff1b34516dcdaf3
Mariela <marieladorta@gmail.com>
2021-01-15 11:28:20 +0000

    Limit player and score

--- Pong/Assets/Scripts/PlayerBehaviourScript.cs
+++ Pong/Assets/Scripts/PlayerBehaviourScript.cs
@@ -8,7 +8,8 @@ public class PlayerBehaviourScript : MonoBehaviour
     public KeyCode upKey;
     public KeyCode downKey;
     public float speed = 6.0f;
-    int limit;
+    float limit = 3.6f;
+    float limitDown = -3.6f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +20,11 @@ public class PlayerBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(upKey)) {
+        if (Input.GetKey(upKey) && limit > transform.position.y) {
             transform.Translate(Vector3.up * speed * Time.deltaTime);
         }
 
-        if (Input.GetKey(downKey))
+        if (Input.GetKey(downKey) && limitDown < transform.position.y)
         {
             transform.Translate(Vector3.down * speed * Time.deltaTime);
         }
